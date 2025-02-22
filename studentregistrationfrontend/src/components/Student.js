96e27a40994b64518393ac8e4e4d26c8915e9f72
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2025-02-22 19:27:32 +0530

    fetch data correctly to the frontend by the backend Signed-off-by: W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>

--- studentregistrationfrontend/src/components/Student.js
+++ studentregistrationfrontend/src/components/Student.js
@@ -1,4 +1,4 @@
-import  React, { useState} from "react";
+import  React, { useEffect, useState} from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Container, Paper } from "@mui/material";
@@ -6,21 +6,29 @@ import Button from "@mui/material/Button";
 
 export default function Student() {
     const paperStyle = { padding: '50px 20px', width: 600, margin: '20px auto' }
-    const [studentName,setStudentName] = useState("");
-    const [studentAddress, setStudentAddress] = useState("");
-    
+    const [name,setName] = useState("");
+    const [address, setAdress] = useState("");
+    const [students, setStudents] = useState([]);
     const handleClick = (e) => { 
-        e.preventDefault();
-        const student = { studentName, studentAddress };
-        fetch("http://localhost:8080/students/add", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(student),
+        e.preventDefault()
+        const student = { name, address };
+        console.log(student);
+        fetch("http://localhost:8080/v1/api/students/add", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(student),
         }).then(() => {
-            console.log("new student added");
-        }
-        );
+          console.log("new student added");
+        });
     }
+    useEffect(() => {
+        fetch("http://localhost:8080/v1/api/students/getAll")
+          .then((response) => response.json())
+          .then((data) => {
+            setStudents(data);
+          });
+    }
+    , []);
     return (
       <Container>
         <Paper elevation={3} style={paperStyle}>
@@ -39,20 +47,28 @@ export default function Student() {
               label="StudentName"
               variant="outlined"
               fullWidth
-              value={studentName}
-              onChange={(e) => setStudentName(e.target.value)}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
             <TextField
               id="outlined-basic"
               label="StudentAddress"
               variant="outlined"
               fullWidth
-              value={studentAddress}
-              onChange={(e) => setStudentAddress(e.target.value)}
+              value={address}
+              onChange={(e) => setAdress(e.target.value)}
             />
             <Button variant="contained" onClick={handleClick} >Submit</Button>
           </Box>
-        </Paper>
+            </Paper>
+            <h1>Students</h1>
+            <Paper elevation={3} style={paperStyle}>
+                {students.map((student) => (<Paper  style={{ padding: "10px", margin: "10px", textAlign: "left" }} key={student.id}>
+                   Id: {student.id} <br />
+                   Name: {student.name} <br />
+                   Address: {student.address}
+                </Paper>))}
+            </Paper>
       </Container>
     );
 }
